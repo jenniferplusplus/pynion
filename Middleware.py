54ac6510245565230c9f3ebfd34c9edacdbe3997
Jennifer Moore <jenniferplusplus@gmail.com>
2021-10-22 01:16:37 -0500

    Added unit tests for Pynion.py

--- Middleware.py
+++ Middleware.py
@@ -7,6 +7,14 @@ sig_err = Callable[[object, object, Optional[sig_eject], Optional[object]], obje
 
 class Middleware:
     def __init__(self, *, pre=sig_handler, post=sig_handler, err=sig_handler, name=str):
+        """
+        :param pre: A function to be executed before the main handler logic. This could, for example perform
+        validations or enrich the event argument before it reaches the handler.
+        :param post: A function to be executed after the main handler logic. It could enrich the result or trigger
+        side-effects.
+        :param err: A function that will have a chance to handle errors raised during processing.
+        :param name: A human readable name for this middleware instance, which may be used in logs or other output.
+        """
         self._pre = pre
         self._post = post
         self._err = err
